ba271111eceb23df6215595fcb462698a866a8cf
haris <hari.wm23@gmail.com>
2023-06-11 04:57:55 +0700

    Update

--- src/components/CountdownTimer .jsx
+++ src/components/CountdownTimer .jsx
@@ -42,7 +42,7 @@ const CountdownTimer = ({ expiredTime, isExpired }) => {
         clearInterval(intervalId);
         setPayment({});
         toast.warn("หมดเวลาชำระเงิน");
-        toast.warn(`${now}`);
+        toast.warn(`${expiredDateTime}`);
         router.replace("/");
       }
     }, 1000);
